9d2f9b2f0b2a5777cdeba8b6e9e1b5175180164f
Paul Joannon <hello@pauljoannon.com>
2015-10-13 11:54:05 +0200

    use .align-left and .align-right as .left and .right

--- static/content-tools/my-content-tools.js
+++ static/content-tools/my-content-tools.js
@@ -7,8 +7,6 @@ window.addEventListener('load', function() {
         ContentTools.StylePalette.add([
             new ContentTools.Style('Chapo', 'chapo', ['p']),
             new ContentTools.Style('Exergue', 'exergue', ['p', 'img']),
-            new ContentTools.Style('Gauche', 'left', ['p', 'img']),
-            new ContentTools.Style('Droite', 'right', ['p', 'img']),
             new ContentTools.Style('Citation', 'quote', ['p']),
             new ContentTools.Style('Question', 'question', ['p']),
             new ContentTools.Style('Appui', 'fold', ['p'])
